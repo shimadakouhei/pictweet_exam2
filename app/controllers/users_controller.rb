339914e5e60d8ba317fa22aa829b2d2ca37e903a
kyohei0423 <kyo199333@gmail.com>
2017-04-28 10:39:30 +0900

    不要な記述の削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,6 @@ class UsersController < ApplicationController
     user = User.find(show_params[:id])
     @nickname = user.nickname
     @tweets = user.tweets.page(params[:page]).per(5).order('created_at DESC')
-    # user = User.find(show_params[:id])
-    # @nickname = user.nickname
-    # @tweets = Tweet.where(user_id: user.id).page(params[:page]).per(5).order('created_at DESC')
   end
 
   private
