6859131b754aa39e46caeedb368094bb83e709a0
kohei-isoda <isodakohei0511@gmail.com>
2019-01-12 01:58:15 +0900

    rails_version_up

--- db/migrate/20141018084902_add_user_id_to_tweets.rb
+++ db/migrate/20141018084902_add_user_id_to_tweets.rb
@@ -1,4 +1,4 @@
-class AddUserIdToTweets < ActiveRecord::Migration
+class AddUserIdToTweets < ActiveRecord::Migration[4.2]
   def change
     add_column :tweets, :user_id, :integer
   end
--- db/migrate/20141112144522_remove_name_from_tweets.rb
+++ db/migrate/20141112144522_remove_name_from_tweets.rb
@@ -1,4 +1,4 @@
-class RemoveNameFromTweets < ActiveRecord::Migration
+class RemoveNameFromTweets < ActiveRecord::Migration[4.2]
   def change
     remove_column :tweets, :name, :string
   end
