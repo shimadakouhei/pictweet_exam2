78d0468af91af9aa24d02f601ff6a83960d45d95
kohei-isoda <isodakohei0511@gmail.com>
2019-01-24 16:50:21 +0900

    change errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "tweet#index"
-  resources :tweets, only: [:index, :shoe, :new, :create, :destroy, :edit, :update]
+  resources :tweets, only: [:index, :shoe, :new, :craete, :destroy, :edit, :update]
   resources :comments, only: [:create]
   resources :users, only: [:show]
 end
